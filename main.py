255994a58414ce3ad7c6b5dc023fbe0a2286601d
EmiWn0 <167633103+EmiWn0@users.noreply.github.com>
2024-04-26 21:05:14 +0300

    Update and rename smsbomber.py to main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ while 1:
     print("""{} 
     S A N D E S
           
-    Sms: {}           {}by {}@emiwn.py tarafından Sandes'e yapılmıştır.\n  
+    Sms: {}           {}by {}@emiwn.py tarafından Sibertim'e yapılmıştır.\n  
     """.format(Fore.LIGHTCYAN_EX, len(servisler_sms), Style.RESET_ALL, Fore.LIGHTRED_EX))
     try:
         menu = (input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder (Normal)\n\n 2- SMS Gönder (Turbo)\n\n 3- Çıkış\n\n" + Fore.LIGHTYELLOW_EX + " Seçim: "))
